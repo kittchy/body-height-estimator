43ffc766b45fe9a14fc3ada77674406d7a33ee74
kittchy <drums.radwimps203@gmail.com>
2024-07-18 00:50:40 +0900

    [FIX] model の回収

--- src/models/components/resnet.py
+++ src/models/components/resnet.py
@@ -68,133 +68,26 @@ class BasicBlock(nn.Module):
         return out
 
 
-class BottleneckBlock(nn.Module):  # bottelneck-block, over the 50 layers.
-    expansion = 4
-
-    def __init__(
-        self,
-        in_channels,
-        hid_channels,
-        use_senet=False,
-        ratio=16,
-        stride=1,
-        downsample=None,
-    ):
-        super(BottleneckBlock, self).__init__()
-        self.downsample = downsample
-        out_channels = hid_channels * self.expansion
-        self.conv1 = conv1x1(in_channels, hid_channels)
-        self.bn1 = nn.BatchNorm2d(hid_channels)
-
-        self.conv2 = conv3x3(hid_channels, hid_channels, stride)
-        self.bn2 = nn.BatchNorm2d(hid_channels)
-
-        self.conv3 = conv1x1(hid_channels, out_channels)
-        self.bn3 = nn.BatchNorm2d(out_channels)
-
-        self.relu = nn.ReLU(inplace=True)
-
-        if use_senet:
-            self.senet = SENet(out_channels, ratio)
-        else:
-            self.senet = None
-
-    def forward(self, x):
-        residual = x  # indentity
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-        out = self.relu(out)
-
-        out = self.conv3(out)
-        out = self.bn3(out)
-
-        if self.downsample is not None:
-            residual = self.downsample(x)
-
-        if self.senet is not None:
-            out = self.senet(out)
-
-        out += residual
-        out = self.relu(out)
-
-        return out
-
-
 class ResNet(nn.Module):
-    """
-    *50-layer
-        conv1 (output: 112x112)
-            7x7, 64, stride 2
-        conv2 (output: 56x56)
-            3x3 max pool, stride 2
-            [ 1x1, 64  ]
-            [ 3x3, 64  ] x 3
-            [ 1x1, 256 ]
-        cov3 (output: 28x28)
-            [ 1x1, 128 ]
-            [ 3x3, 128 ] x 4
-            [ 1x1, 512 ]
-        cov4 (output: 14x14)
-            [ 1x1, 256 ]
-            [ 3x3, 256 ] x 6
-            [ 1x1, 1024]
-        cov5 (output: 28x28)
-            [ 1x1, 512 ]
-            [ 3x3, 512 ] x 3
-            [ 1x1, 2048]
-        _ (output: 1x1)
-            average pool, 100-d fc, softmax
-        FLOPs 3.8x10^9
-    """
-
-    """
-    *101-layer
-        conv1 (output: 112x112)
-            7x7, 64, stride 2
-        conv2 (output: 56x56)
-            3x3 max pool, stride 2
-            [ 1x1, 64  ]
-            [ 3x3, 64  ] x 3
-            [ 1x1, 256 ]
-        cov3 (output: 28x28)
-            [ 1x1, 128 ]
-            [ 3x3, 128 ] x 4
-            [ 1x1, 512 ]
-        cov4 (output: 14x14)
-            [ 1x1, 256 ]
-            [ 3x3, 256 ] x 23
-            [ 1x1, 1024]
-        cov5 (output: 28x28)
-            [ 1x1, 512 ]
-            [ 3x3, 512 ] x 3
-            [ 1x1, 2048]
-        _ (output: 1x1)
-            average pool, 100-d fc, softmax
-        FLOPs 7.6x10^9
-    """
-
     def __init__(
         self,
-        use_bottleneck_block: bool,
         layers,
         num_classes=1,
+        input_channel_num=4,  # depth, pose, mask, original image
         use_senet=False,
         ratio=16,
     ):
         super(ResNet, self).__init__()
         self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
+        self.input_channel_num = input_channel_num
 
         self.layers = layers
-        self.in_channels = 64
+        self.in_channels = 64 + self.input_channel_num
         self.use_senet = use_senet
         self.ratio = ratio
 
         self.conv1 = nn.Conv2d(
-            in_channels=4,  # depth, pose, mask, original image
+            in_channels=self.input_channel_num,
             out_channels=64,
             kernel_size=7,
             stride=2,
@@ -204,7 +97,7 @@ class ResNet(nn.Module):
         self.bn = nn.BatchNorm2d(64)
         self.relu = nn.ReLU(inplace=True)
         self.maxPool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        block = BottleneckBlock if use_bottleneck_block else BasicBlock
+        block = BasicBlock
         self.conv2 = self.get_layers(block, 64, self.layers[0])
         self.conv3 = self.get_layers(block, 128, self.layers[1], stride=2)
         self.conv4 = self.get_layers(block, 256, self.layers[2], stride=2)
@@ -239,7 +132,7 @@ class ResNet(nn.Module):
                 downsample,
             )
         )
-        self.in_channels = hid_channels * block.expansion
+        self.in_channels = hid_channels * block.expansion + self.input_channel_num
 
         for _ in range(1, n_layers):
             layers.append(
@@ -251,6 +144,7 @@ class ResNet(nn.Module):
         """
         Example tensor shape based on resnet101
         """
+        original_x = x
         x = self.conv1(x)
         x = self.bn(x)
         x = self.relu(x)
@@ -273,16 +167,3 @@ def resnet18(**kwargs):
 
 def resnet34(**kwargs):
     return ResNet(False, [3, 4, 6, 3], **kwargs)
-
-
-def resnet50(**kwargs):
-    return ResNet(True, [3, 4, 6, 3], **kwargs)
-
-
-def resnet101(**kwargs):
-    """ResNet-101 Model"""
-    return ResNet(True, [3, 4, 23, 3], **kwargs)
-
-
-def resnet152(**kwargs):
-    return ResNet(True, [3, 8, 36, 3], **kwargs)
